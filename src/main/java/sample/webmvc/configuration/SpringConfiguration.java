28450981e4086cbce32f26d971b6b38b13a2f390
mauryarajan782-design <mauryarajan782@gmail.com>
2026-09-21 14:27:33 +0530

    update

---
+++ src/main/java/sample/webmvc/configuration/SpringConfiguration.java
@@ -0,0 +1,29 @@
+package sample.webmvc.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+@EnableWebMvc
+@ComponentScan(basePackages = "sample.webmvc")
+@Configuration
+public class SpringConfiguration implements WebMvcConfigurer {
+
+	@Bean
+	public ViewResolver viewResolver() {
+		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+// It specifies that JSTL views should be used.
+//JstlView is a class provided by the Spring framework that is specifically designed to handle JSP pages using JSTL.
+		viewResolver.setViewClass(JstlView.class);
+		viewResolver.setPrefix("/WEB-INF/JSP/");
+		viewResolver.setSuffix(".jsp");
+
+		return viewResolver;
+	}
+
+}
